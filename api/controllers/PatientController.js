dfaca5ff242dce7877eb96edc6747feeb093a4df
jennysheng <qd_sheng2000@hotmail.com>
2019-05-22 17:38:57 +0200

    TempleWebApp_mongodb

---
+++ api/controllers/PatientController.js
@@ -0,0 +1,30 @@
+/**
+ * PatientController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `PatientController.medicine()`
+   */
+  medicine: async function (req, res) {
+    return res.json({
+      todo: 'medicine() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PatientController.sleepingpattern()`
+   */
+  sleepingpattern: async function (req, res) {
+    return res.json({
+      todo: 'sleepingpattern() is not implemented yet!'
+    });
+  }
+
+};
+
---
+++ api/models/Patient.js
@@ -0,0 +1,32 @@
+/**
+ * Patient.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    medicine: { type: 'string' },
+
+    sleepingpattern: { type: 'string' }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
